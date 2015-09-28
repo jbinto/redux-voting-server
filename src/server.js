2540f7318b3ae3e81b30759f62750c67a7991931
Jesse Buchanan <hello@jessebuchanan.ca>
2015-09-28 00:06:15 -0400

    startServer: Subscribe to Redux store, emit all state changes to Websocket

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  // Subscribe to the Redux store.
+  //
+  // Whenever our application state changes, we will send
+  // the entire state to all connected websocket clients.
+  //
+  // This could certainly be optimized further.
+
+  store.subscribe(
+    // Using top level `io.emit` emits to all clients
+    () => io.emit('state', store.getState().toJS())
+  );
 }
