815df5b31fdfc7965a4e80423dfd6939fff3f462
Jesse Buchanan <hello@jessebuchanan.ca>
2015-09-27 23:56:18 -0400

    "npm run start" will now run a socket.io server on port 8090

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // index.js is the entry point of our server application.
 import makeStore from './src/store';
+import startServer from './src/server';
 
 export const store = makeStore();
+startServer();
