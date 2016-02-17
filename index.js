dd56e3aa64f9d7a5b8a619942a8c042ea236a106
Jesse Buchanan <hello@jessebuchanan.ca>
2016-02-17 15:34:34 -0500

    Final commit from Nov 28.
    * Add cities, and shuffle them.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // index.js is the entry point of our server application.
 import makeStore from './src/store';
 import startServer from './src/server';
+import arrayShuffle from 'array-shuffle';
 
 export const store = makeStore();
 startServer(store);
+
+let entries = require('./entries.json');
 
 // Now that the server is started, populate it with some default entries.
 //  n.b. Really neat to see that you can `require` a JSON file like this.
 store.dispatch({
   type: 'SET_ENTRIES',
-  entries: require('./entries.json')
+  entries: arrayShuffle(entries)
 });
 
 store.dispatch({
